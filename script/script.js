582dcda0517c795d1da275d4f4c1bfa2205fe92c
JTthePres <jacopo.tamarri@outlook.it>
2023-04-22 11:11:42 +0200

    gg

--- script/script.js
+++ script/script.js
@@ -1,22 +1,18 @@
-function taskOptions() {
-    var current_tasks = document.querySelectorAll(".taskOption");
-    // for(var i=0; i<current_tasks.length; i++){
-    //     current_tasks[i].onclick = function(){
-    //         this.parentNode.hiddenOption.style.display="block";
-
-    //     }
-    // }
-    for (var i=0; i<current_tasks.length; i++) {
-        current_tasks[i].addEventListener("click", function() {
-          //this.classList.toggle("active");
-          var content = this.nextElementSibling;
-          if (content.style.display === "block") {
-            content.style.display = "none";
-          } else {
-            content.style.display = "block";
-          }
-        });
-    }}
+// function taskOptions() {
+//     var current_tasks = document.querySelectorAll(".taskOption");
+//     for (var i=0; i<current_tasks.length; i++) {
+//       current_tasks[i].addEventListener("click", function() {
+//         this.classList.toggle("active"); // aggiunto
+//         var content = this.nextElementSibling;
+//         if (content.style.display === "block") {
+//           content.style.display = "none";
+//         } else {
+//           content.style.display = "block";
+//         }
+//       });
+//     }
+//   }
+  
 function addTask(){
     if(document.querySelector('#newtask input').value.length == 0){
       alert("Kindly Enter Task Name!!!!")
@@ -31,12 +27,12 @@ function addTask(){
               </span>
               <span  >
               ${document.getElementById("pomoTaskNumber").value}</span>
-              <button class="taskOption" onclick="taskOptions();">
+              <button type="button" class="taskOption" >
               </button>
               </div>
             <br>
             <div class="hiddenOption">
-                <div> Hello world!</div>
+              <p>  Hello world!</p>
             </div>
       `;
       document.getElementById("taskFieldInput").value="";
@@ -46,5 +42,29 @@ function addTask(){
               this.parentNode.remove();
           }
       }
+    //   var current_tasks = document.querySelectorAll(".taskOption");
+    //   for(var i=0; i<current_tasks.length; i++){
+    //     current_tasks[i].onclick = function(){
+    //          this.nextElementSibling.remove();
+            
+    //     }
+    // }
+    var coll = document.getElementsByClassName("taskOption");
+var i;
+for (i = 0; i < coll.length;i++) {
+  console.log(i)
+console.log(coll[i])
+  coll[i].addEventListener("click", function() {
+    //this.classList.toggle("active");
+    console.log(i)
+    console.log(coll);
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
   }
 }
