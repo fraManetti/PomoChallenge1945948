9df18e8e85bd06c293fb3113d5bac7c40b5a881b
JTthePres <jacopo.tamarri@outlook.it>
2023-04-22 12:56:33 +0200

    jj

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
- 
+function updateTaskBox (taskItems,  cond){
+  if (!cond){
+    taskItems[1].setAttribute("readonly","readonly");
+  }
+  else{
+    taskItems[1].removeAttribute("readonly");
+  }
+}
 function addTask(){
     if(document.querySelector('#newtask input').value.length == 0){
       alert("Kindly Enter Task Name!!!!")
   }else{
+    var number= parseInt(document.getElementById("pomoTaskNumber").value);
+    console.log(typeof number);
       document.querySelector('#tasks').innerHTML += `
           <div class="task">
               <button class="delete">
@@ -10,8 +19,7 @@ function addTask(){
               </button>
               <input type="text" readOnly id="taskname" value=" ${document.querySelector('#newtask input').value}">
                  
-              <span  >
-              ${document.getElementById("pomoTaskNumber").value}</span>
+              <input type="number" value="" min=0 id="x" >
               <button type="button" class="taskOption" >
               </button>
             <div class="hiddenOption">
@@ -19,6 +27,7 @@ function addTask(){
             </div>
             </div>
       `;
+      document.getElementById("x").value=number;
       document.getElementById("taskFieldInput").value="";
       var current_tasks = document.querySelectorAll(".delete");
       for(var i=0; i<current_tasks.length; i++){
@@ -32,17 +41,19 @@ var i;
 for (i = 0; i < coll.length;i++) {
   coll[i].addEventListener("click", function() {
     var taskBox = this.parentNode;
+    var taskItems =taskBox.children
     //console.log(taskBox);
     var hiddenBox = this.nextElementSibling;
     //console.log(hiddenBox);
     if (hiddenBox.style.display === "block") {
         taskBox.classList.toggle("taskShowed");
         hiddenBox.style.display = "none";
+        updateTaskBox(taskItems,false);
     } else {
       hiddenBox.style.display = "block";
       //taskBox.height("100px");
       taskBox.classList.toggle("taskShowed");
-      taskBox
+      updateTaskBox(taskItems,true);
     }
   });
 }
