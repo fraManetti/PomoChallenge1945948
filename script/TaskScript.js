271a03af8ca7ce33fb9820e3848dc0700040ef20
Francesco Manetti <manetti.1945948@studenti.uniroma1.it>
2023-04-30 09:57:35 +0200

    swap

--- script/TaskScript.js
+++ script/TaskScript.js
@@ -292,8 +292,7 @@ function addTask(){
 
     document.querySelector("#swapclick").addEventListener("click", function() {
       i1 = document.getElementById("index1").value;
-      i2 = document.getElementById("index2").value;
-      
+      i2 = document.getElementById("index2").value;  
   });
   }
 
