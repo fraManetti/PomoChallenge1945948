4de880e203542d551fe78eef3c086ae3990efb9a
adevecchis <a.devecchis01@gmail.com>
2023-05-30 18:53:09 +0200

    prova

--- script/reportScript/reportScript.js
+++ script/reportScript/reportScript.js
@@ -12,6 +12,188 @@ var mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
 var myChart = null;
 //canvas.width = window.innerWidth * 1;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function load(s, e) {
+  var currentActiveButton = document.querySelector('.tabClass.active');
+  if (currentActiveButton) {
+    currentActiveButton.classList.remove('active');
+  }
+  e.currentTarget.classList.add('active');
+
+  document.querySelector("#currentPeriod").innerHTML= "";
+  document.querySelector("#totalTime").innerHTML= "";
+  currentD = new Date();
+  if(myChart!= null) myChart.destroy();
+  var url;
+    totalTime = 0 ;
+  if(s == 'daily') {
+    document.querySelector("#currentPeriod").innerText= currentString;
+    url = "../server/dailyLoad.php";
+    currentPeriodType = "day";
+    currentHour = new Date().getHours();
+    hourCharts(currentHour);
+    document.getElementById("increaseTimePeriod").disabled = false;
+    document.getElementById("decreaseTimePeriod").disabled = false;
+
+    var avgBtns = document.querySelectorAll(".avgBtnClass");
+    avgBtns.forEach(function(btn) {
+        btn.style.display = "none";
+    });
+  }
+  else if(s == 'weekly') {
+    document.querySelector("#currentPeriod").innerText= mon + " - " + sun;
+    url = "../server/weeklyLoad.php";
+    currentPeriodType = "week";
+    weekCharts();
+    document.getElementById("increaseTimePeriod").disabled = false;
+    document.getElementById("decreaseTimePeriod").disabled = false;
+    weekInterval(currentD);
+    checkWeekBorder();
+
+    var avgBtns = document.querySelectorAll(".avgBtnClass");
+    avgBtns.forEach(function(btn) {
+        btn.style.display = "none";
+    });
+  }
+  else if(s == 'monthly') {
+    url = "../server/monthlyLoad.php";
+    currentPeriodType = "month";
+    currentMonth = currentD.getMonth()+1;
+    document.querySelector("#currentPeriod").innerText= mesi[currentMonth-1];
+    monthCharts(currentMonth);
+    document.getElementById("increaseTimePeriod").disabled = false;
+    document.getElementById("decreaseTimePeriod").disabled = false;
+    checkMonthsBorder();
+
+    var avgBtns = document.querySelectorAll(".avgBtnClass");
+    avgBtns.forEach(function(btn) {
+        btn.style.display = "none";
+    });
+  }
+  else if(s == 'all') {
+    url = "../server/allLoad.php";
+    currentPeriodType = "none";
+    document.querySelector("#currentPeriod").innerText= "Total tasks";
+    document.getElementById("increaseTimePeriod").disabled = true;
+    document.getElementById("decreaseTimePeriod").disabled = true;
+    avgDailyCharts();
+
+    var avgBtns = document.querySelectorAll(".avgBtnClass");
+    avgBtns.forEach(function(btn) {
+        btn.style.display = "block";
+    });
+    document.getElementById("avgWeek").classList.remove('active');
+    document.getElementById("avgDay").classList.add('active');
+  }
+  document.getElementById("tasksPanel").innerHTML = '';
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.open("GET", url, true);
+  httpRequest.setRequestHeader('Content-Type', 'application/json');
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+      var response = JSON.parse(httpRequest.responseText);
+      if ('error' in response) {
+        console.log(response.error);
+      } else {
+        totalTime=0;
+          response.forEach(function(tuple) {
+            downloadEnded(tuple);
+            totalTime+= JSON.parse(tuple.tim);
+        });
+        upTotalTime(totalTime);
+        
+      }
+    }
+  };
+  httpRequest.send();
+}
+
+
+
+
+
+
+function weekCharts() {
+  if (myChart) {
+    myChart.destroy();
+  }
+  const ctx = document.getElementById('myChartCanvas').getContext("2d");
+  weekQuery().then((data) => {
+    const weekLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    myChart = new myChart(ctx, {
+      type: 'bar',
+      data: {
+        labels: weekLabels,
+        datasets: [{
+          label: `# minutes in a week`,
+          data: [data[0][1],data[1][1],data[2][1],data[3][1],data[4][1],data[5][1],data[6][1]],
+          borderWidth: 0.8,
+          backgroundColor: "#e7645d",
+          hoverBackgroundColor: "#fc9690",
+        }]
+      },
+      options: {
+      
+        normalized: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+            grace: '5%'
+          }
+        }
+      }
+    });
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
+
+
+function weekQuery() {
+  return new Promise((resolve, reject) => {
+    const url = "../server/getWeekTime.php";
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url, true);
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        const response = JSON.parse(httpRequest.responseText);
+        if ('error' in response) {
+          reject(response.error);
+        } else {
+          resolve(response);
+        }
+      }
+    }
+    httpRequest.send();
+  });
+}
+
+
+
+
+
 function hourCharts() {
   const ctx = document.getElementById('myChartCanvas');
   dailyQuery().then((data) => {
@@ -33,6 +215,7 @@ function hourCharts() {
         }]
       },
       options: {
+        responsive: true,
         showLine: true,
         borderWidth: '2px',
         normalized: true,
@@ -173,40 +356,7 @@ function monthCharts(i) {
   });
 }
 
-function weekCharts() {
-  if (myChart) {
-    myChart.destroy();
-  }
-  const ctx = document.getElementById('myChartCanvas').getContext("2d");
-  weekQuery().then((data) => {
-    const weekLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: weekLabels,
-        datasets: [{
-          label: `# minutes in a week`,
-          data: [data[0][1],data[1][1],data[2][1],data[3][1],data[4][1],data[5][1],data[6][1]],
-          borderWidth: 0.8,
-          backgroundColor: "#e7645d",
-          hoverBackgroundColor: "#fc9690",
-        }]
-      },
-      options: {
-      
-        normalized: true,
-        scales: {
-          y: {
-            beginAtZero: true,
-            grace: '5%'
-          }
-        }
-      }
-    });
-  }).catch((error) => {
-    console.error(error);
-  });
-}
+
 
 function weekCharts2(s) {
   if (myChart) {
@@ -337,25 +487,7 @@ function monthQuery() {
   });
 }
 
-function weekQuery() {
-  return new Promise((resolve, reject) => {
-    const url = "../server/getWeekTime.php";
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", url, true);
-    httpRequest.setRequestHeader('Content-Type', 'application/json');
-    httpRequest.onreadystatechange = function() {
-      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-        const response = JSON.parse(httpRequest.responseText);
-        if ('error' in response) {
-          reject(response.error);
-        } else {
-          resolve(response);
-        }
-      }
-    }
-    httpRequest.send();
-  });
-}
+
 
 function weekQuery2(s) {
   return new Promise((resolve, reject) => {
@@ -603,103 +735,7 @@ function endedOption(e) {
     }
 }
 
-function load(s, e) {
-  var currentActiveButton = document.querySelector('.tabClass.active');
-  if (currentActiveButton) {
-    currentActiveButton.classList.remove('active');
-  }
-  e.currentTarget.classList.add('active');
 
-  document.querySelector("#currentPeriod").innerHTML= "";
-  document.querySelector("#totalTime").innerHTML= "";
-  currentD = new Date();
-  if(myChart!= null) myChart.destroy();
-  var url;
-    totalTime = 0 ;
-  if(s == 'daily') {
-  /*  document.querySelector("#currentPeriod").insertAdjacentHTML('beforeend', `
-    <p1> ${currentString} <br></p1>
-  `);*/
-    document.querySelector("#currentPeriod").innerText= currentString;
-    url = "../server/dailyLoad.php";
-    currentPeriodType = "day";
-    currentHour = new Date().getHours();
-    hourCharts(currentHour);
-    document.getElementById("increaseTimePeriod").disabled = false;
-    document.getElementById("decreaseTimePeriod").disabled = false;
-
-    var avgBtns = document.querySelectorAll(".avgBtnClass");
-    avgBtns.forEach(function(btn) {
-        btn.style.display = "none";
-    });
-  }
-  else if(s == 'weekly') {
-    document.querySelector("#currentPeriod").innerText= mon + " - " + sun;
-    url = "../server/weeklyLoad.php";
-    currentPeriodType = "week";
-    weekCharts();
-    document.getElementById("increaseTimePeriod").disabled = false;
-    document.getElementById("decreaseTimePeriod").disabled = false;
-    weekInterval(currentD);
-    checkWeekBorder();
-
-    var avgBtns = document.querySelectorAll(".avgBtnClass");
-    avgBtns.forEach(function(btn) {
-        btn.style.display = "none";
-    });
-  }
-  else if(s == 'monthly') {
-    url = "../server/monthlyLoad.php";
-    currentPeriodType = "month";
-    currentMonth = currentD.getMonth()+1;
-    document.querySelector("#currentPeriod").innerText= mesi[currentMonth-1];
-    monthCharts(currentMonth);
-    document.getElementById("increaseTimePeriod").disabled = false;
-    document.getElementById("decreaseTimePeriod").disabled = false;
-    checkMonthsBorder();
-
-    var avgBtns = document.querySelectorAll(".avgBtnClass");
-    avgBtns.forEach(function(btn) {
-        btn.style.display = "none";
-    });
-  }
-  else if(s == 'all') {
-    url = "../server/allLoad.php";
-    currentPeriodType = "none";
-    document.querySelector("#currentPeriod").innerText= "Total tasks";
-    document.getElementById("increaseTimePeriod").disabled = true;
-    document.getElementById("decreaseTimePeriod").disabled = true;
-    avgDailyCharts();
-
-    var avgBtns = document.querySelectorAll(".avgBtnClass");
-    avgBtns.forEach(function(btn) {
-        btn.style.display = "block";
-    });
-    document.getElementById("avgWeek").classList.remove('active');
-    document.getElementById("avgDay").classList.add('active');
-  }
-  document.getElementById("tasksPanel").innerHTML = '';
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.open("GET", url, true);
-  httpRequest.setRequestHeader('Content-Type', 'application/json');
-  httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-      var response = JSON.parse(httpRequest.responseText);
-      if ('error' in response) {
-        console.log(response.error);
-      } else {
-        totalTime=0;
-          response.forEach(function(tuple) {
-            downloadEnded(tuple);
-            totalTime+= JSON.parse(tuple.tim);
-        });
-        upTotalTime(totalTime);
-        
-      }
-    }
-  };
-  httpRequest.send();
-}
 
 function chartLoad(s, e) {
   var currentActiveButton = document.querySelector('.avgBtnClass.active');
